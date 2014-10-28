ebb3964421b0253cccd5d87d4c31184ad9faea42
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-10-28 23:33:32 +0000

    first commit

---
+++ 8.groovy
@@ -0,0 +1,14 @@
+// 8 Palindrome
+
+print "Enter your text: "
+String s = System.console().readLine()
+int sLength = s.length()
+int pointer = sLength - 1
+char sChar 
+print s
+	while (pointer != 0) {
+		sChar = s.charAt(pointer)
+		printer sChar
+	}
+println "palindrome y'all"
+		
