517b707919862f382e37395f0b4afc086056cbf2
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-10-28 23:43:51 +0000

    solved

---
+++ 9.groovy
@@ -0,0 +1,16 @@
+// 9 Palindrome creator
+
+print "Enter your text: "
+String s = System.console().readLine()
+int sLength = s.length()
+int pointer = sLength - 1
+char sChar 
+print s
+	while (pointer != -1) {
+		sChar = s.charAt(pointer)
+		print sChar
+		pointer--
+	}
+print "\n"
+println "palindrome y'all"
+		
