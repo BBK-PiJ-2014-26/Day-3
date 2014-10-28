d8d59a498c6410e2c29252d4011c03dd4875d9fb
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-10-28 22:42:30 +0000

    First commit and first part of solution

---
+++ 5.groovy
@@ -0,0 +1,15 @@
+// 5 Counting letters
+
+print "Enter a line of text: "
+String s = System.console().readLine()
+int countE = 0
+int pointer = 0
+int sLength = s.length()
+While (pointer) != (sLength - 1)) {
+	char letter = s.charAt(pointer)
+	if (letter = e || letter = E) {
+		countE = countE + 1
+		pointer++
+	} else { 
+		pointer++
+println "There are " + pointer + "e characters"
